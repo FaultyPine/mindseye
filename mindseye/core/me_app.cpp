cbb786fbe8a53617d017553187d559a921413d82
FaultyPine <gclark1013@gmail.com>
2026-02-03 22:33:50 -0800

    oop

--- mindseye/core/me_app.cpp
+++ mindseye/core/me_app.cpp
@@ -99,7 +99,7 @@ static void InitializeEngineConfig(EngineContext* engine)
 	}
 	else
 	{
-		SerializeFromFile(userProjectConfigPath, &engine->engineArena, TD_MEUSERCONFIG, SPAN_FROM(engine->userConfig)));
+		SerializeFromFile(userProjectConfigPath, &engine->engineArena, TD_MEUSERCONFIG, SPAN_FROM(engine->userConfig));
 		// "userApp" referring to a program that uses the mindseye engine
 		StringView userAppConfigFile = engine->userConfig.projectRootConfigFile;
 		String userAppConfigPathAbs = meOSResolveRelativeToAbsPath(GetTLScratch(), userAppConfigFile);
