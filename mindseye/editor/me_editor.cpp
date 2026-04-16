56059103107746941294913b95744ca4b0a7bc1b
FaultyPine <gclark1013@gmail.com>
2026-04-15 20:59:19 -0700

    shortcut

--- mindseye/editor/me_editor.cpp
+++ mindseye/editor/me_editor.cpp
@@ -235,7 +235,7 @@ void meEditorTick(EngineContext* engine)
         
         if (editor.sceneDirty)
         {
-            if (ImGui::Button("Save Current Scene (Ctrl+S)") || (ImGui::IsKeyDown(ImGuiMod_Ctrl) && ImGui::IsKeyDown(ImGuiKey_S)))
+            if (ImGui::Button("Save Current Scene (Ctrl+S)") || (ImGui::Shortcut(ImGuiMod_Ctrl | ImGuiKey_S)))
             {
                 meExternalCommand cmd = {};
                 cmd.type = meExternalCommandType_SaveCurrentScene;
